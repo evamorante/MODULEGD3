3f0ee0be57307de394dc7fef8f58451c48461fd6
EvaDM <evamorante1969@gmail.com>
2023-04-19 12:53:20 +0200

    coding on manipulations on arrays

---
+++ manipulationsOnArrays.py
@@ -0,0 +1,24 @@
+songs = ["Highway for Heaven", "Viens je t'emmène", "La corrida"]
+print(songs)
+
+# exercice 04. module 8
+print(songs[0])
+print(songs[1])
+print(songs[2])
+#list index out of range
+#print(songs[3])
+
+# exercise 06. module 8
+misterG = ["Steeve", "Temblenton", 38, 152, True]
+misterZ = ["Eduard", "Redstar", 45, 178, False]
+missisA = ["Rose", "Zimermann", 34, 168, True]
+print(misterG[1])
+print(misterZ[1])
+print(missisA[1])
+
+#ajouter
+#enlever
+#remplacer
+#trier
+#inverser l'ordre
+#mesurer la longueur (nombre d'entrées de data)
