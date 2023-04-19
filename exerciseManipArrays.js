b6f749356d700daf8035a6c80fe1dcd4f15cbf7e
EvaDM <evamorante1969@gmail.com>
2023-04-19 15:05:35 +0200

    creating html file for this folder

---
+++ exerciseManipArrays.js
@@ -0,0 +1,14 @@
+/** 
+    Create a JavaScript array to store your 3 favorite dish.
+    List each of them in the console on their own separate line.
+    Display the count of your favorite dishes.
+    Add a 4th dish to the array.
+    Display the count once more within a sentence such as "I have x favorite dishes."
+    Remove the 2nd one!
+    Sort them in alphabetical order.
+    Display them once more but this time as an array in the console.
+    Display the count again!
+*/
+
+var dishes = ["fondue", "raclette", "couscous"];
+console.log(dishes);
---
+++ manipulationsOnArrays.js
@@ -0,0 +1,10 @@
+var fruits = ["oranges", "bananas", "strawberry", "apple"];
+
+/**afficher
+ajouter 
+enlever
+remplacer
+trier
+inverser
+longueur (nombre d'entrées de data)
+*/
